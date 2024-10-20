8a238f38bd20d183bcee30842c6f8e466e77a084
Nashruful <azooz.n40@hotmail.com>
2024-10-20 21:14:09 +0300

    Starting

--- apps/user_app/lib/main.dart
+++ apps/user_app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:components/component/text_test.dart';
+import 'package:user_app/screens/home_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -11,11 +11,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: TextTest(),
-        ),
-      ),
+      home: HomeScreen(),
     );
   }
 }
---
+++ apps/user_app/lib/setup/setup.dart
@@ -0,0 +1,14 @@
+import 'package:get_it/get_it.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:user_app/data_layer/data_layer.dart';
+
+
+
+final getIt = GetIt.instance;
+
+Future<void> setup() async{
+  await GetStorage.init();
+  getIt.registerSingleton<DataLayer>(DataLayer());
+
+
+}
