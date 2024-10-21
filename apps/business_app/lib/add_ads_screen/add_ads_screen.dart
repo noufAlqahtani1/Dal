1a31460a26127a2485adf2bbd6f4fdf6b344fe57
Amjad Elahi <amjadelahi07@gmail.com>
2024-10-21 21:56:49 +0300

    implements add ads screen

---
+++ apps/business_app/lib/add_ads_screen/add_ads_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AddAdsScreen extends StatelessWidget {
+  const AddAdsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xffA51361),
+        foregroundColor: const Color(0xffF7F7F7),
+        title: const Text('Add Ads'),
+      ),
+      body: const Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          children: [
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
