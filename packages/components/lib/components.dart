6037a2be74b03f65fd90c61c47e16dda467b607e
Amjad Elahi <amjadelahi07@gmail.com>
2024-10-21 14:42:38 +0300

    update components

--- packages/components/lib/components.dart
+++ packages/components/lib/components.dart
@@ -6,4 +6,6 @@ export 'package:components/component/text_test.dart';
 export 'package:components/component/custom_text/custom_text.dart';
 export 'package:components/component/custom_buttons/custom_icon_button.dart';
 export 'package:components/component/custom_cintainers/custom_ads_container.dart';
+export 'package:components/component/custom_bottom_sheets/custom_bottom_sheet.dart';
+
 
