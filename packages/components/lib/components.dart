6197708b9b889f2a1ae61ca7edf9bb0ec813dcd0
Nashruful <azooz.n40@hotmail.com>
2024-10-23 11:25:05 +0300

    before merging

--- packages/components/lib/components.dart
+++ packages/components/lib/components.dart
@@ -2,7 +2,6 @@ library components;
 
 // Export components here --
 
-export 'package:components/component/text_test.dart';
 export 'package:components/component/custom_text/custom_text.dart';
 export 'package:components/component/custom_buttons/custom_elevated_button.dart';
 export 'package:components/component/custom_buttons/custom_icon_button.dart';
