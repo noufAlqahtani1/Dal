c02e2240c9f7b5d22ec5bb6f746ea15ec2d66c65
Amjad Elahi <amjadelahi07@gmail.com>
2024-10-24 11:24:45 +0300

    update

--- apps/business_app/lib/screens/profile_screen/profile_screen.dart
+++ apps/business_app/lib/screens/profile_screen/profile_screen.dart
@@ -1,7 +1,8 @@
 import 'package:components/components.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:user_app/screens/profile_screen/bloc/profile_bloc_bloc.dart';
+
+import 'bloc/profile_bloc_bloc.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
