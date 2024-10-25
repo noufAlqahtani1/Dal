7f5fe39997bd8d7bbfb793afcfb6acf5ba12852b
Linda <linda@Lindas-MacBook-Pro.local>
2024-10-25 16:46:12 +0300

    check out from lindda

--- apps/user_app/lib/main.dart
+++ apps/user_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:components/component/theme/theme.dart';
 import 'package:device_preview_plus/device_preview_plus.dart';
 import 'package:flutter/material.dart';
+import 'package:user_app/screens/auth_screens/login_screen.dart';
 import 'package:user_app/screens/bottom_nav_bar_screen/bottom_nav_bar_screen.dart';
 
 void main() {
@@ -20,7 +21,7 @@ class MainApp extends StatelessWidget {
       theme: AppThemes.lightTheme,
       darkTheme: AppThemes.darkTheme,
       themeMode: ThemeMode.system,
-      home: const BottomNavBarScreen(),
+      home: const LoginScreen(),
     );
   }
 }
