422172d663dd8e7c361d12691a268ccd7fa04973
Nashruful <azooz.n40@hotmail.com>
2024-10-25 17:52:04 +0300

    before merging nouf

--- packages/components/lib/component/custom_containers/custom_ads_container.dart
+++ packages/components/lib/component/custom_containers/custom_ads_container.dart
@@ -33,7 +33,13 @@ class CustomAdsContainer extends StatelessWidget {
         ]),
         child: Stack(
           children: [
-            Image.network(companyLogo,fit: BoxFit.fill,height: 160,),
+            Positioned.fill(
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(8.0),
+              child: Image.asset(companyLogo,fit: BoxFit.fill,)),
+            ),
+          
+////
             Positioned(
               top: 0,
               right: 0,
