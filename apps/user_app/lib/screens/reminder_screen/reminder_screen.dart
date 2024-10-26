37547fcbcfd411206322b1ff026957ccc1432cfa
Linda <linda@Lindas-MacBook-Pro.local>
2024-10-26 11:30:35 +0300

    modified reminder screen

---
+++ apps/user_app/lib/screens/reminder_screen/reminder_screen.dart
@@ -0,0 +1,42 @@
+import 'package:components/component/custom_app_bar/custom_app_bar.dart';
+import 'package:components/component/custom_containers/custom_ads_container.dart';
+import 'package:components/component/custom_text/custom_text.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:user_app/data_layer/data_layer.dart';
+import 'package:user_app/setup/setup.dart';
+
+class ReminderScreen extends StatelessWidget {
+  const ReminderScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CustomAppBar(
+        title: "My Reminders".tr(),
+      ),
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: MediaQuery.of(context).size.width > 600 ? 3 : 2,
+          crossAxisSpacing: 16,
+          mainAxisSpacing: 16,
+          childAspectRatio: 2 / 3,
+        ),
+        itemCount: getIt.get<DataLayer>().myReminders.length,
+        itemBuilder: (context, index) {
+          final item = getIt.get<DataLayer>().myReminders;
+
+          return Padding(
+            padding: const EdgeInsets.only(top: 12),
+            child: CustomAdsContainer(
+              companyLogo: item[index]['bannerimg'],
+              remainingDay: '4d',
+              companyName: item[index]['title'],
+              offers: item[index]['offer_type'],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
