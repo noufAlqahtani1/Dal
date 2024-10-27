9b7108113b9caf657588e80a3e2e0714cdd026f2
Amjad Elahi <amjadelahi07@gmail.com>
2024-10-27 23:44:03 +0300

    integrate getIt library with business app

--- apps/business_app/lib/main.dart
+++ apps/business_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:business_app/screens/bottom_nav_bar_screen/bottom_nav_bar_screen.dart';
 import 'package:business_app/services/supabase/supabase_configration.dart';
+import 'package:business_app/setup/setup.dart';
 import 'package:components/component/theme/theme.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:easy_localization/easy_localization.dart';
@@ -9,6 +10,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
   await SupabaseConfigration.connectSupabase();
+  await setup();
   runApp(
     DevicePreview(
       enabled: true,
