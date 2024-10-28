910177bc123ffcff72672a546c87a12650d79f37
Nashruful <azooz.n40@hotmail.com>
2024-10-28 11:11:29 +0300

    before merging amjad

--- apps/user_app/lib/screens/discover_screen/bloc/discover_bloc.dart
+++ apps/user_app/lib/screens/discover_screen/bloc/discover_bloc.dart
@@ -10,11 +10,13 @@ part 'discover_state.dart';
 
 class DiscoverBloc extends Bloc<DiscoverEvent, DiscoverState> {
   Position? position;
-  final LatLng initialCenter = LatLng(24.82307363899628, 46.76960924937112);
-    LatLng currentLocation = LatLng(24.82307363899628, 46.76960924937112);
+  final LatLng initialCenter =
+      const LatLng(24.82307363899628, 46.76960924937112);
+  double distancee = 1000;
+  bool buttonClicked = false;
 
   // List of provided markers
-  final List<LatLng> markerLocations = [
+  final List<LatLng> markerLocations = const [
     LatLng(24.81307299899741, 46.76503970781533),
     LatLng(24.82037851123455, 46.76296974609059),
     LatLng(24.81924369998163, 46.77445218221551),
@@ -29,39 +31,35 @@ class DiscoverBloc extends Bloc<DiscoverEvent, DiscoverState> {
     });
     on<LoadScreenEvent>((event, emit) async {
       emit(LoadingState());
-      final LocationSettings locationSettings = LocationSettings();
+      final LocationSettings locationSettings = const LocationSettings();
       position = await Geolocator.getCurrentPosition(
           locationSettings: locationSettings);
-          
-    
 
-    // Create markers from the provided locations and filter them
-    filteredMarkers = markerLocations
-        .map((location) {
-          double distance = Geolocator.distanceBetween(
-            currentLocation.latitude,
-            currentLocation.longitude,
-            location.latitude,
-            location.longitude,
-          );
+      // Create markers from the provided locations and filter them
+      filteredMarkers = markerLocations
+          .map((location) {
+            double distance = Geolocator.distanceBetween(
+              initialCenter.latitude,
+              initialCenter.longitude,
+              location.latitude,
+              location.longitude,
+            );
 
-          // Only include markers within 300 meters
-          return distance <= 1000
-              ? Marker(
-                  width: 100,
-                  height: 100,
-                  point: location,
-                  child: const Icon(
-                      color: Colors.blueAccent,
-                      size: 35,
-                      Icons.location_on_rounded))
-              : null;
-        })
-        .whereType<Marker>()
-        .toList();
+            // Only include markers within 300 meters
+            return distance <= distancee
+                ? Marker(
+                    width: 100,
+                    height: 100,
+                    point: location,
+                    child: const Icon(
+                        color: Colors.blueAccent,
+                        size: 35,
+                        Icons.location_on_rounded))
+                : null;
+          })
+          .whereType<Marker>()
+          .toList();
 
-    
-  
       emit(SuccessState());
     });
   }
